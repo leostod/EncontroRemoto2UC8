02c7b9b35b72a97ce7a0386d9041a84dc49af9ae
leostod <leo.stod@gmail.com>
2022-02-28 00:23:40 -0300

    primeiro commit

---
+++ Remoto2.js
@@ -0,0 +1,16 @@
+let numeroDeAlunos = 10
+
+for (let index = 0; index <= numeroDeAlunos; index++)
+{
+    
+//console.log (index)
+
+if (index == 0) {
+    console.log("Número atual é zero.")
+} else if (index % 2 == 0) {
+    console.log("Número " + index + " é par.")
+} else {
+    console.log("Número " + index + " é ímpar.")
+}
+
+}
